3fcfdce1813b6383c38f5eadf64855822838f2b6
heartcadence <info@heartcadence.com>
2026-01-24 20:27:18 +0000

    Optimize Build Architecture

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'winter-primary': '#1e40af',
+        'summer-primary': '#15803d',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
